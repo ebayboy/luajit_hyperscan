80872b1ab94794f49a9cdbdae65cbcb8455d943b
ebayboy <ebayboy@163.com>
2020-09-09 14:15:12 +0800

    fw

--- examples/build_simple.sh
+++ examples/build_simple.sh
@@ -2,4 +2,4 @@
 
 #g++ -std=c++11 simplegrep.c ../lib/libhs.a -I ../include/ -o simplegrep
 
-g++ -O3 -std=c++17 simplegrep_multi.c ../lib/libhs.a -I ../include/ -o simplegrep_multi
+g++ -O3 -std=c++17 simplegrep_multi.c ../lib/libhs.a -I ../include/ -o simplegrep_multi /usr/local/gperftools/lib/libtcmalloc_minimal.a
