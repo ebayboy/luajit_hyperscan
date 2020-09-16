fef12ccaddb63f877904d8e149d5a87d2212f126
ebayboy <ebayboy@163.com>
2020-09-17 02:05:44 +0800

    fw

---
+++ single_include/gtest/main.cc
@@ -0,0 +1,16 @@
+/****************************************************************************
+@file:test-util.cpp 
+@author:ebayboy@163.com 
+@date:2020-08-20 16:22 
+@version: 1.0  
+@description: cpp file 
+@Copyright (c)  all right reserved 
+**************************************************************************/
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv)
+{
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
---
+++ single_include/gtest/test.cc
@@ -0,0 +1,30 @@
+/****************************************************************************
+@file:test-util.cpp 
+@author:ebayboy@163.com 
+@date:2020-08-20 16:22 
+@version: 1.0  
+@description: cpp file 
+@Copyright (c)  all right reserved 
+**************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <fstream>
+
+#include <gtest/gtest.h>
+
+using namespace std;
+
+#include "hpsfilter.hpp"
+
+//#include unit test header
+
+#if 0
+TEST(protobuf, test_write)
+{
+	ASSERT_TRUE(test_write() == 0);
+}
+#endif
+
